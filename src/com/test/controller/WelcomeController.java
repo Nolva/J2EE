40505b554a86c10734ec8f4b719fe562fc55349d
h-hx <hhx14@qq.com>
2019-05-28 23:45:45 +0800

    ^.^

--- src/com/test/controller/WelcomeController.java
+++ src/com/test/controller/WelcomeController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WelcomeController {
 
-
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
