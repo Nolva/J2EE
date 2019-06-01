8429c99c312c46ad3312559c4a3fe1d2665389d1
h-hx <hhx14@qq.com>
2019-06-01 19:13:02 +0800

    修复一些小问题:

---
+++ src/main/java/com/test/interceptor/LoginInterceptor.java
@@ -0,0 +1,58 @@
+package com.test.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+//登录拦截器
+public class LoginInterceptor  implements HandlerInterceptor {
+
+    public String[] allowUrls;//还没发现可以直接配置不拦截的资源，所以在代码里面来排除
+
+    public void setAllowUrls(String[] allowUrls) {
+        this.allowUrls = allowUrls;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String requestUrl = request.getRequestURI().replace(request.getContextPath(), "");
+        System.out.println(requestUrl);
+        if (null != allowUrls && allowUrls.length >= 1) {
+            for (String url : allowUrls) {
+                if (requestUrl.contains(url)) {
+                    return true;
+                }
+            }
+        }
+
+        HttpSession session = request.getSession();
+        System.out.println(session.getAttribute("manager"));
+        if(session.getAttribute("manager") != null){
+            return true;
+        }
+
+        //执行到这里时表示用户身份需要认证，跳转到登陆页面
+        request.getRequestDispatcher("tologin").forward(request, response);
+        return false;
+
+    }
+
+    //进入Handler方法之后，返回modelAndView之前执行
+    //应用场景从modelAndView出发：将公用的模型数据（比如菜单导航）在这里传到视图，也可以在这里统一指定视图
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println(" LoginInterceptor..postHandle");
+    }
+
+    //执行Handler完成后执行此方法
+    //应用场景：统一异常处理，统一日志处理
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        System.out.println("LoginInterceptor..afterCompletion");
+    }
+
+}
